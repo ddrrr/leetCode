d3fe1dec2a23632acc2fed8d29dc198e78edb685
朱晓丹 <zhuxiaodan@zhuxiaodandeMacBook-Pro.local>
2020-05-02 10:13:08 +0800

    无重复字符的最长子串 ，比较简单

---
+++ 3m.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let maxLength = 0;
+    let chrArr = [];
+    for(let i = 0;i < s.length;i++){
+        let chr = s.charAt(i);
+        let findIdx = chrArr.indexOf(chr);
+        if(findIdx == -1){
+            chrArr.push(chr);
+            maxLength = Math.max(maxLength,chrArr.length);
+        }
+        else{
+            chrArr.splice(0,findIdx+1);
+            chrArr.push(chr);
+        }
+    }
+
+    return maxLength;
+};
+
+console.log(lengthOfLongestSubstring('abcabcbb'));
