6dbfaf71919f7a1b8a2f95a238b256241fa1f30d
ddr <122495279@qq.com>
2020-05-09 11:09:17 +0800

    组合总和。dfs剪枝，参考了答案

---
+++ 39m.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function (candidates, target) {
+    candidates.sort((a,b) => a - b);   
+    let finalResult = [];
+    finalResult.push
+
+    
+    _getComb(candidates,target,0,[],finalResult);
+    return finalResult;
+};
+
+var _getComb = function (candidates, leftValue,begin,path,finalResult) {
+    if(leftValue == 0){
+        finalResult.push([...path]);
+        return;
+    }
+
+    let len = candidates.length;
+    for(let i = begin;i < len;i++){
+        if(leftValue - candidates[i] < 0){
+            break;
+        }
+
+        path.push(candidates[i]);
+        _getComb(candidates,leftValue- candidates[i],i,path,finalResult);
+        path.pop();
+    }
+}
+
+console.log(combinationSum([2, 3, 5], 8));
