79a9e0bb5aa5d11f81e2458cd0793ba76fb0b289
朱晓丹 <zhuxiaodan@zhuxiaodandeMacBook-Pro.local>
2020-05-10 17:27:39 +0800

    又优化了下迭代

--- 49m.js
+++ 49m.js
@@ -4,25 +4,21 @@
  */
 var groupAnagrams = function(strs) {
     let resultArray = [];
-    let newArray = [];
     let resultMap = new Map();
     //先把每个字符串排序
+    //把字符串一样的序号放进一个hash里
     for(let i = 0;i < strs.length;i++){
         let perstr = strs[i];
         let chrArray = perstr.split('');
         chrArray.sort((a,b)=>a.charCodeAt(0) - b.charCodeAt(0));
         perstr = chrArray.join();
-        newArray.push(perstr);
-    }
 
-    //把字符串一样的序号放进一个hash里
-    for(let i = 0;i < newArray.length;i++){
-        let mapArray = resultMap.get(newArray[i]);
+        let mapArray = resultMap.get(perstr);
         if(!mapArray){
             mapArray = [];
         }
         mapArray.push(strs[i]);
-        resultMap.set(newArray[i],mapArray);
+        resultMap.set(perstr,mapArray);
     }
 
     //再把答案拿出来
